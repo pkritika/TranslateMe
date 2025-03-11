27de6d87ac02729631687b92a6299025e295a226
pkritika <kritika.pant@bison.howard.edu>
2025-03-10 21:15:50 -0400

    Initial Commit

---
+++ TranslateMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranslateMe
+//
+//  Created by Kritika  on 3/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Kritika  on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
